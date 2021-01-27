03803b2b42b67c02b896c840c09a1f06fba7a03c
stephanie-ai <stephanie_cooper@hotmail.com>
2021-01-27 10:50:05 +0000

    add styling and link back to homepage

--- url_shortener/shorten_app/views.py
+++ url_shortener/shorten_app/views.py
@@ -6,19 +6,6 @@ import random, string
 from .forms import EnterUrlForm
 from .models import Url
 
-# Create your views here.
-# def enter(request):
-#     if request.method == 'POST':
-#         url = EnterUrlForm(request.POST)
-#         if url.is_valid():
-#             url_id = url.save().id
-#             return redirect("url-show", url_id=url_id)
-#     else:
-#         form = EnterUrlForm()
-#     data = {'form': form }
-#     return render(request, 'shorten_app/form.html', data)
-    # return render(request, 'shorten_app/form.html')
-
 
 def redirect(request, url):
     current_obj = Url.objects.filter(short_url=url)
@@ -46,62 +33,5 @@ def enter(request):
     
     else:
         form = EnterUrlForm()
-        context = {'form': form}
-        return render(request, 'shorten_app/form.html', context)
-
-
-
-# users enter a url
-# def enter(request):
-#     if request.method == 'POST':
-#         url = EnterUrlForm(request.POST)
-#         if url.is_valid():
-#             url_id = url.save().id
-#             return redirect("url-show", url_id=url_id)
-#     else:
-#         form = EnterUrlForm()
-#     data = {'form': form }
-#     return render(request, 'shorten_app/form.html', data)
-
-
-# show the new shortened url
-# def show(request, url_id):
-#     url = get_object_or_404(Url, pk=url_id)
-#     if request.method == "POST":
-#         form = EnterUrlForm(request.POST)
-#         if form.is_valid():
-#             url.save()
-#             return redirect('url-show', url_id=url_id)
-#     else:
-#         form = EnterUrlForm(initial={'url'})
-#     data = {
-#         'url': url,
-#         'form': form
-#     }
-#     return render(request, 'shorten_app/newUrl.html', data)
-
-
-# def urlShort(request):
-#     if request.method == 'POST':
-#         form = Url(request.POST)
-#         pdb.set_trace()
-#         if form.is_valid():
-#             short = ''.join(random.choice(string.ascii_letters)
-#                            for x in range(10))
-#             url = form.cleaned_data["url"]
-#             new_url = UrlData(url=url, short=short)
-#             new_url.save()
-#             pdb.set_trace()
-#             request.user.urlshort.add(new_url)
-#             return redirect("url-short", url_id=url_id)
-#             return render(request, 'shorten_app/newUrl.html', {"url": new_url})
-         
-#     else:
-#         form = Url()
-#     data = UrlData.objects.all()
-#     context = {
-#         'form': form,
-#         'data': data
-#     }
-#     return render(request, 'shorten_app/form.html', context)
-
+    context = {'form': form}
+    return render(request, 'shorten_app/form.html', context)
