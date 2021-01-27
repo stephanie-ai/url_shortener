459f480d097d97a6b40588afdf91a7b524edf803
stephanie-ai <stephanie_cooper@hotmail.com>
2021-01-27 09:42:18 +0000

    create enterurlform

--- url_shortener/shorten_app/forms.py
+++ url_shortener/shorten_app/forms.py
@@ -1,20 +1,11 @@
-# from django.forms import ModelForm
-# from django import forms
-from .models import UrlData
-# class EnterUrlForm(forms.ModelForm):
-
-#     class Meta:
-#         model = Url
-#         fields = [
-#             'url', 
-#             'shorten'
-#         ]
-#         widgets = {'url': forms.HiddenInput()}
-
-
-#  <p>here is your url shortened {{}}</p>
-
-
 from django import forms
-class Url(forms.Form):
-    url = forms.CharField(label="URL")
+from .models import Url
+
+class EnterUrlForm(forms.ModelForm):
+    class Meta:
+        model = Url
+        fields = ['original_url']
+
+        widgets = {
+            'original_url': forms.TextInput(attrs={'class': 'form-control'})
+        }
